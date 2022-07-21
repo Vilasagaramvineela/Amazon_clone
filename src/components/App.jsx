5cda9ac38c398087945d2c73b817ecad9e248739
Vilasagaramvineela <86571199+Vilasagaramvineela@users.noreply.github.com>
2022-07-21 18:28:48 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Banner from "./Banner";
+import Product from "./Product";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Banner />
+      <Product />
+    </div>
+  );
+}
+
+export default App;
